a4d50015b5c7f3a19707c4f76e350dca7b14aa79
allansantana021 <ssantanallan@gmail.com>
2025-11-27 12:02:21 -0300

    Salvando...

--- src/main/java/ratelimiting/controller/TestController.java
+++ src/main/java/ratelimiting/controller/TestController.java
@@ -1,4 +1,4 @@
-package ratelimiting;
+package ratelimiting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/ratelimiting/filter/RateLimitFilter.java
+++ src/main/java/ratelimiting/filter/RateLimitFilter.java
@@ -1,4 +1,4 @@
-package ratelimiting;
+package ratelimiting.filter;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
@@ -9,33 +9,51 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest; // Importação necessária
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap; // Importação para Mapa Concorrente
 
 @Component
 public class RateLimitFilter implements Filter {
 
-    // 5 requisições por minuto
-    private final Bucket bucket = Bucket.builder()
-            .addLimit(
-                    Bandwidth.classic(
-                            5,                             // quantidade permitida
-                            Refill.intervally(5, Duration.ofMinutes(1)) // renova a cada minuto
-                    )
-            )
-            .build();
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+
+    private Bucket createNewBucket() {
+        Bandwidth limit = Bandwidth.classic(
+                5,
+                Refill.intervally(5, Duration.ofMinutes(1))
+        );
+        return Bucket.builder().addLimit(limit).build();
+    }
+
+
+    private Bucket getBucket(String key) {
+
+        return cache.computeIfAbsent(key, k -> createNewBucket());
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        if (bucket.tryConsume(1)) {
-            // permite continuar
+
+        String clientIp = request.getRemoteAddr();
+
+
+        Bucket ipBucket = getBucket(clientIp);
+
+
+        if (ipBucket.tryConsume(1)) {
+
             chain.doFilter(request, response);
         } else {
-            // bloqueia
+
             HttpServletResponse http = (HttpServletResponse) response;
             http.setStatus(429); // Too Many Requests
             http.getWriter().write("Limite de requisições excedido. Tente novamente em instantes.");
